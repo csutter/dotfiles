ad9d095a6b804cc58525aa0e75c67852aa447223
Christian Sutter <git@doublesignal.com>
2025-12-17 16:14:48 +0000

    fish: Tweak prompt colors; set up git info

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,3 +5,23 @@ set -gx VISUAL hx
 
 # No welcome message
 set -g fish_greeting
+
+# Prompt colors
+set -g fish_color_cwd brwhite --bold
+set -g fish_color_user brwhite
+
+# Git prompt settings
+set -g __fish_git_prompt_showdirtystate 1
+set -g __fish_git_prompt_showuntrackedfiles 1
+set -g __fish_git_prompt_showstashstate 1
+
+set -g __fish_git_prompt_color_branch normal
+set -g __fish_git_prompt_color_stagedstate green
+set -g __fish_git_prompt_color_dirtystate yellow
+set -g __fish_git_prompt_color_untrackedfiles red
+set -g __fish_git_prompt_color_stashstate magenta
+
+set -g __fish_git_prompt_char_stagedstate '+'
+set -g __fish_git_prompt_char_dirtystate '*'
+set -g __fish_git_prompt_char_untrackedfiles '?'
+set -g __fish_git_prompt_char_stashstate '[S]'
