8816fd41770829c8e98f2782fdeb2ad5cf2ba172
Christian Sutter <git@doublesignal.com>
2025-12-21 15:00:44 +0000

    env: Add Docker Desktop to PATH on macOS

--- tag-macos/config/fish/conf.d/00-macos.fish
+++ tag-macos/config/fish/conf.d/00-macos.fish
@@ -5,3 +5,6 @@ set -gx HOMEBREW_NO_ANALYTICS 1
 
 ## 1Password SSH agent
 set -gx SSH_AUTH_SOCK "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+
+## Docker Desktop
+set -gx PATH $HOME/.docker/bin $PATH
