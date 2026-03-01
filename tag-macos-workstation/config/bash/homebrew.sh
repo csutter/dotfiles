f420cc6bef03aa15746f61056c3f435066dc7fd5
Christian Sutter <git@doublesignal.com>
2026-03-01 15:14:55 +0000

    macOS: remove Docker Desktop; split bash configs

---
+++ tag-macos-workstation/config/bash/homebrew.sh
@@ -0,0 +1,3 @@
+# shellcheck shell=bash
+export PATH="/opt/homebrew/bin:$PATH"
+export HOMEBREW_NO_ANALYTICS=1
--- tag-macos-workstation/config/bash/ssh_agent.sh
+++ tag-macos-workstation/config/bash/ssh_agent.sh
@@ -1,7 +1,2 @@
 # shellcheck shell=bash
-export PATH="/opt/homebrew/bin:$PATH"
-export HOMEBREW_NO_ANALYTICS=1
-
 export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
-
-export PATH="$HOME/.docker/bin:$PATH"
