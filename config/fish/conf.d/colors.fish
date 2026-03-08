0eb78716e39d45fe86fd1a1bde18bca2684ad234
Christian Sutter <git@doublesignal.com>
2026-03-08 15:10:55 +0000

    Assorted colour tweaks

--- config/fish/conf.d/colors.fish
+++ config/fish/conf.d/colors.fish
@@ -1,18 +1,18 @@
 # Syntax highlighting colors
-set -g fish_color_normal white
-set -g fish_color_command white
-set -g fish_color_keyword white
-set -g fish_color_quote white
-set -g fish_color_redirection white
-set -g fish_color_end white
-set -g fish_color_error white
-set -g fish_color_param '#ffffff'
-set -g fish_color_comment white
-set -g fish_color_operator white
-set -g fish_color_escape white
-set -g fish_color_autosuggestion '#454545'
+set -g fish_color_normal brwhite
+set -g fish_color_command brwhite
+set -g fish_color_keyword brwhite
+set -g fish_color_quote '#bae6ff'
+set -g fish_color_redirection brwhite
+set -g fish_color_end brwhite
+set -g fish_color_error brwhite
+set -g fish_color_param brwhite
+set -g fish_color_comment '#878d96'
+set -g fish_color_operator brwhite
+set -g fish_color_escape brwhite
+set -g fish_color_autosuggestion '#4d5358'
 set -g fish_color_valid_path
-set -g fish_color_selection --background=brblack
+set -g fish_color_selection --background="#012749"
 set -g fish_color_search_match --background=brblack
 
 # Pager colors (tab completions)
