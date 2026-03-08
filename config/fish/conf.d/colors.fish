2dc4f7dcfabecc45efa5b52afeb1b9420471b00b
Christian Sutter <git@doublesignal.com>
2026-03-08 11:52:22 +0000

    Play around with colours a bit

---
+++ config/fish/conf.d/colors.fish
@@ -0,0 +1,22 @@
+# Syntax highlighting colors
+set -g fish_color_normal white
+set -g fish_color_command white
+set -g fish_color_keyword white
+set -g fish_color_quote white
+set -g fish_color_redirection white
+set -g fish_color_end white
+set -g fish_color_error white
+set -g fish_color_param '#ffffff'
+set -g fish_color_comment white
+set -g fish_color_operator white
+set -g fish_color_escape white
+set -g fish_color_autosuggestion '#454545'
+set -g fish_color_valid_path
+set -g fish_color_selection --background=brblack
+set -g fish_color_search_match --background=brblack
+
+# Pager colors (tab completions)
+set -g fish_pager_color_prefix white
+set -g fish_pager_color_completion white
+set -g fish_pager_color_description white
+set -g fish_pager_color_selected_background --background=brblack
